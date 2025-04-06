35531a31e0587039e97d080da28e2abe95c63b1d
Johnston Liu <johnstonliu2004@gmail.com>
2025-04-06 13:28:34 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
